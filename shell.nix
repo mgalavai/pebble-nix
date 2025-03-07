657465870aee5780c9ad9ff9416ee84c8dcaaad4
Your Name <myroslav.galavai@gmail.com>
2025-03-07 22:57:29 +0100

    GROK15

--- shell.nix
+++ shell.nix
@@ -1,1 +1,10 @@
-(import (builtins.fetchTarball "https://github.com/Sorixelle/pebble.nix/archive/master.tar.gz")).pebbleEnv { } 
+let
+  pkgs = import <nixpkgs> {};
+  pebbleEnv = (import (builtins.fetchTarball "https://github.com/Sorixelle/pebble.nix/archive/master.tar.gz")).pebbleEnv { };
+in
+pebbleEnv.overrideAttrs (old: {
+  buildInputs = old.buildInputs ++ [
+    pkgs.freetype
+    pkgs.python27Packages.freetype-py  # Python 2.7 bindings for freetype
+  ];
+})
